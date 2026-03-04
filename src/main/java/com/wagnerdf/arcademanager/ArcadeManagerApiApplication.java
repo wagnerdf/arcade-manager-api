6ee9dd2fc2fd1177b4fa594dd92aab8f82b8ce01
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-03-04 18:55:50 -0300

    feat: add Spring Boot initial structure with MongoDB

---
+++ src/main/java/com/wagnerdf/arcademanager/ArcadeManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerdf.arcademanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcadeManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerdf/arcademanager/ArcadeManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerdf.arcademanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadeManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
