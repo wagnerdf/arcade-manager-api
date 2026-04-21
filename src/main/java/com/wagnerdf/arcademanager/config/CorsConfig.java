ecc5f979dc4f2288e84424c0eb2228118dd5e2eb
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-04-20 21:00:49 -0300

    feat: enable CORS for frontend integration

---
+++ src/main/java/com/wagnerdf/arcademanager/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.wagnerdf.arcademanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8081"); // frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
